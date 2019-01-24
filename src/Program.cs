f0e3e0a92a246b856a6873d7423955056177e62f
Rodrigo Campos <rodigopscampos@gmail.com>
2019-01-24 21:54:45 -0200

    Adiciona ao repositorio

---
+++ src/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace aspnetcoremvc
+{
+    public class Program
+    {
+        public static readonly Dictionary<string, string> _argsMap = 
+        new Dictionary<string, string>
+        {
+            { "-h", "Host" },
+            { "-p", "Port" }
+        };
+
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) => {
+                    config.AddCommandLine(args, _argsMap);
+                })
+                .UseStartup<Startup>();
+    }
+}
